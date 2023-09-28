befb440e718d23782859c9290850479471e4b8ce
Kevin Galligan <kgalligan@gmail.com>
2023-09-28 07:23:16 -0400

    Fixes for cocoapods

--- allshared/build.gradle.kts
+++ allshared/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     kotlin("multiplatform")
     id("co.touchlab.kmmbridge")
     id("co.touchlab.skie")
+    kotlin("native.cocoapods")
     `maven-publish`
 }
 
@@ -10,16 +11,23 @@ kotlin {
     @Suppress("OPT_IN_USAGE")
     targetHierarchy.default()
 
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64()
-    ).forEach {
-        it.binaries.framework {
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    cocoapods {
+        summary = "KMMBridgeSKIETemplate"
+        homepage = "https://www.touchlab.co"
+        ios.deploymentTarget = "13.5"
+        extraSpecAttributes["libraries"] = "'c++', 'sqlite3'"
+        license = "BSD"
+        extraSpecAttributes.put("swift_version", "\"5.0\"") // <- SKIE Needs this!
+        framework {
             export(project(":analytics"))
             isStatic = true
         }
     }
+
 
     sourceSets {
         val commonMain by getting {
@@ -36,5 +44,5 @@ addGithubPackagesRepository()
 kmmbridge {
     mavenPublishArtifacts()
     spm()
-//    cocoapods("git@github.com:touchlab/PodSpecs.git")
+    cocoapods("git@github.com:touchlab/PodSpecs.git")
 }
