afb442af34659c611b1dd87d02499f4264951e54
Kevin Galligan <kgalligan@gmail.com>
2023-10-12 10:20:48 -0400

    Remove snapshot repos

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,6 @@ allprojects {
     repositories {
         google()
         mavenCentral()
-        maven("https://oss.sonatype.org/content/repositories/snapshots")
     }
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,9 +3,6 @@ pluginManagement {
         google()
         gradlePluginPortal()
         mavenCentral()
-        maven {
-            url = java.net.URI("https://oss.sonatype.org/content/repositories/snapshots")
-        }
     }
 }
 
